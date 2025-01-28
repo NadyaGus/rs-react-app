73ce16bc073d39f517343a01b77cbdb6974bcf59
Nadya Gusakova <ftotkexit@gmail.com>
2025-01-28 21:17:53 +0300

    refactor: add prettier

--- modules.d.ts
+++ modules.d.ts
@@ -1,2 +1,2 @@
-declare module "eslint-plugin-react-compiler";
-declare module "eslint-plugin-react-hooks";
+declare module 'eslint-plugin-react-compiler';
+declare module 'eslint-plugin-react-hooks';
--- src/api/showData.ts
+++ src/api/showData.ts
@@ -1,9 +1,9 @@
 async function showData(): Promise<void> {
-    await fetch("https://api.jikan.moe/v4/anime?q=naruto&limit=10&page=1")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }
+  await fetch('https://api.jikan.moe/v4/anime?q=naruto&limit=10&page=1')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
 
-  export { showData };
+export { showData };
