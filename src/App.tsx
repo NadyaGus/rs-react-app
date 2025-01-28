58afbff2adafdd5749141a150bc3b2a3c33d86ff
Nadya Gusakova <ftotkexit@gmail.com>
2025-01-28 21:17:08 +0300

    refactor: remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,12 @@
-import type { ReactElement } from "react";
+import type { ReactElement } from 'react';
 
-import { useState } from "react";
-
-import reactLogo from "./assets/react.svg";
-
-import viteLogo from "/vite.svg";
-
-import "./App.css";
+import './App.css';
+import { showData } from './api/showData';
 
 function App(): ReactElement {
-  const [count, setCount] = useState(0);
-
-  async function showData(): Promise<void> {
-    await fetch("https://api.jikan.moe/v4/anime?q=naruto&limit=10&page=1")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => showData()}>Show Data</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => showData()}>Show Data</button>
     </>
   );
 }
