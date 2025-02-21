66bc33bdfe65394e8b7ed308d9a31515275f1732
Nadya Gusakova <ftotkexit@gmail.com>
2025-02-21 22:28:32 +0300

    fix: update tests setup

--- src/__test__/setup-test.ts
+++ src/__test__/setup-test.ts
@@ -1,12 +1,14 @@
 import '@testing-library/jest-dom/vitest';
 
-import { afterAll, afterEach, beforeAll } from 'vitest';
+import { afterAll, afterEach, beforeAll, vi } from 'vitest';
 
 import { setupServer } from 'msw/node';
 
 import { handlers } from './mock/handlers';
 
 export const server = setupServer(...handlers);
+
+window.URL.createObjectURL = vi.fn();
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
