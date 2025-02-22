6b85b211c3c5a1d53cde82075782a7b42eb230fa
Nadya Gusakova <ftotkexit@gmail.com>
2025-02-22 22:01:16 +0300

    feat: update csv create function

--- src/components/favorites/createCSV.ts
+++ src/components/favorites/createCSV.ts
@@ -2,21 +2,20 @@ import { CardProps } from '../../types/cardTypes';
 
 export const createCSV = (data: CardProps[]): string => {
   const titles =
-    ['title_english', 'title_japanese', 'link', 'source', 'status'].join(',') +
-    '\n';
+    ['title_english', 'title_japanese', 'link', 'source', 'status'].join(';') +
+    '\r\n';
 
-  const values = data
-    .map((card) => {
-      console.log(card.synopsis);
-      return [
+  const values = data.map((card) => {
+    return (
+      [
         card.title_english,
         card.title_japanese,
         card.url,
         card.source,
         card.status,
-      ];
-    })
-    .join('\n');
+      ].join(';') + '\r\n'
+    );
+  });
 
   return titles + values;
 };
